7b562605f03516b0bd02ebe5b79088cf147452cb
yavol <yavol@smile.fr>
2015-09-26 13:50:29 +0300

    fixed login/pass  saving

--- app/src/main/java/com/xyrality/gmbh/app/ChangeCredentialsDialogFragment.java
+++ app/src/main/java/com/xyrality/gmbh/app/ChangeCredentialsDialogFragment.java
@@ -3,6 +3,7 @@ package com.xyrality.gmbh.app;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,8 +31,15 @@ public class ChangeCredentialsDialogFragment extends DialogFragment {
             public void onClick(View v) {
                 EditText loginEditText = (EditText) view.findViewById(R.id.loginEditText);
                 EditText passEditText = (EditText) view.findViewById(R.id.passEditText);
-                UserAccount.getInstance().setLogin(loginEditText.getText().toString());
-                UserAccount.getInstance().setPassword(passEditText.getText().toString());
+                String newLogin = loginEditText.getText().toString();
+                if (newLogin != null && !newLogin.isEmpty()) {
+                    UserAccount.getInstance().setLogin(newLogin);
+                }
+                String newPass = passEditText.getText().toString();
+                if (newPass != null && !newPass.isEmpty()) {
+                    UserAccount.getInstance().setPassword(newPass);
+                }
+                Log.d("loginpass", UserAccount.getInstance().getLogin() + ", " + UserAccount.getInstance().getPassword());
                 ((ActivityGameWorldsList) getActivity()).startGameWorldsLoader(false);
                 dismiss();
             }
